04c236d04b467dc2ea1a73f5d357f029a2c84542
t.komori <t.komori@relicwoods.jp>
2022-07-04 22:26:56 +0900

    サンプル関数をそれっぽい無意味で重いやつに設定。

--- CUISample/Program.cs
+++ CUISample/Program.cs
@@ -1,7 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using NLog;
+
+
+
 namespace CUISample // Note: actual namespace depends on the project name.
 {
     internal class Program
@@ -12,38 +19,80 @@ namespace CUISample // Note: actual namespace depends on the project name.
         static Logger logger = LogManager.GetCurrentClassLogger();
 
 
+        static int Count { get; set; }
+        static int Max { get; set; }
+        static int Min { get; set; }
+        static string FileName { get; set; }
         /// <summary>
         /// 引数解析を行うCommandLineUtilsに処理を渡す。
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
     {
-            var user = Environment.GetEnvironmentVariable("BASIC_AUTH_USER_NAME");
-            var pass = Environment.GetEnvironmentVariable("BASIC_AUTH_PASSWORD");
-            var googleCredential = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
-            var jobName = Environment.GetEnvironmentVariable("AWS_JOB_QUEUE_NAME");
-            var jobRoleARN = Environment.GetEnvironmentVariable("AWS_JOB_EXECUTE_ROLE_ARN");
 
-
-      
-            if(user == null || user == "" ||
-               pass == null || pass == "" ||
-               googleCredential == null || googleCredential == "" ||
-               jobName == null || jobName == "" ||
-               jobRoleARN == null || jobRoleARN == "")
+            try
             {
-                logger.Error("環境変数が適切に設定されていません");
+                Count= Convert.ToInt32(Environment.GetEnvironmentVariable("LIST_COUNT"));
+                Max = Convert.ToInt32(Environment.GetEnvironmentVariable("RANGE_MAX"));
+                Min = Convert.ToInt32(Environment.GetEnvironmentVariable("RANGE_MIN"));
+                FileName = Environment.GetEnvironmentVariable("FILE_NAME") ?? "test.txt";
 
-                logger.Error("BASIC_AUTH_USER_NAME:{0}", user);
-                logger.Error("BASIC_AUTH_PASSWORD:{0}", pass);
-                logger.Error("GOOGLE_APPLICATION_CREDENTIALS:{0}", googleCredential);
-                logger.Error("AWS_JOB_QUEUE_NAME:{0}", jobName);
-                logger.Error("AWS_JOB_EXECUTE_ROLE_ARN:{0}", jobRoleARN);
+                if (Count == 0)
+                {
+                    logger.Error("環境変数が適切に設定されていません");
+                    Count = 100000;
+                }
+                if(Max < Min || (Max == 0 && Min == 0))
+                {
+                    Max = int.MaxValue;
+                    Min = int.MinValue;
+                }
 
-                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex,"環境変数が適切に設定されていません");
+                Count = 10000;
+                Max = int.MaxValue;
+                Min = int.MinValue;
+
+                //throw;
             }
 
-            //Add Applcation code
+
+            //Application code
+            var mid = SlowFunction(Count, Min, Max,FileName);
+            Console.WriteLine(mid);
+
+        }
+
+        /// <summary>
+        /// ランダムな配列を作成してソートしてファイルに書き出す。
+        /// </summary>
+        /// <param name="count"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <param name="path"></param>
+        /// <returns>だいたい真ん中のカズ</returns>
+        static int SlowFunction(int count,int min,int max,string path)
+        {
+            var rand = new Random();
+            var list = Enumerable.Range(0, Count)
+                                 .Select(x => rand.Next(min, max))
+                                 .ToList();
+            list.Sort();
+
+            using (var wr = new StreamWriter(path: path,
+                                                      append: false,
+                                                      encoding: System.Text.Encoding.UTF8))
+            {
+                foreach (var num in list)
+                {
+                    wr.WriteLine(num);
+                }
+            }
+
+            return list[(int)(list.Count/2)];
 
 
         }
