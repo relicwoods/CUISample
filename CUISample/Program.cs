6bf8072c64b6058518b30cd77c443bb640d22708
t.komori <t.komori@relicwoods.jp>
2022-07-04 21:02:09 +0900

    プロジェクト ファイルを追加します。

---
+++ CUISample/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace CUISample // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        => CommandLineApplication.Execute<Program>(args);
+
+        [Option]
+
+
+
+        private void OnExecute()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+
